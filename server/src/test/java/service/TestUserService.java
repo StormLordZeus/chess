a2bc5a33d8ca0a205368bd81aad838667f24e66b
Christopher Zaugg <czauggt@byu.edu>
2025-10-09 18:57:45 -0600

    Implemented the UserService tests

---
+++ server/src/test/java/service/TestUserService.java
@@ -0,0 +1,99 @@
+package service;
+
+import dataaccess.DataAccessException;
+import model.*;
+
+public class TestUserService
+{
+    private String mAuthToken;
+
+    public static void main(String[] args) {
+        TestUserService myTester = new TestUserService();
+        UserService tester = new UserService();
+
+        // Test Register
+        System.out.println("Testing UserService. Begin by registering StormLordZeus");
+        myTester.TestRegister(tester);
+        System.out.println("Attempting to re-register StormLordZeus");
+        myTester.TestRegister(tester);
+        System.out.println();
+
+        // Test Login
+        System.out.println("Logging in as StormLordZeus with the correct password");
+        myTester.TestLogin(tester, "StormLordZeus", "I'm really cool");
+        System.out.println("Logging in as StormLordZeus with an incorrect password");
+        myTester.TestLogin(tester, "StormLordZeus", "I'm not so cool");
+        System.out.println("Logging in as an unknown user");
+        myTester.TestLogin(tester, "LameoNotZeus", "I'm really cool");
+        System.out.println();
+
+        // Test Logout
+        System.out.println("Logging StormLordZeus out");
+        myTester.TestLogout(tester);
+        System.out.println("Attempting to log StormLordZeus out again");
+        myTester.TestLogout(tester);
+        System.out.println();
+
+        // Test clear
+        System.out.println("Testing clear users");
+        myTester.TestClearUsers(tester);
+        System.out.println("Proving users have been cleared by attempting to log in as StormLordZeus with correct password");
+        myTester.TestLogin(tester, "StormLordZeus", "I'm really cool");
+        System.out.println();
+
+        System.out.println("Testing of UserService complete");
+    }
+
+    public TestUserService()
+    {
+
+    }
+
+    public void TestRegister(UserService tester)
+    {
+        try
+        {
+            RegisterRequest requestTest = new RegisterRequest(
+                    "StormLordZeus", "I'm really cool", "myEmailIsBetterThanYours@ImAwesome.com");
+            RegisterResult result = tester.register(requestTest);
+            System.out.println("Successfully registered " + result.username() + " With auth token: " + result.authToken());
+
+        }
+        catch (DataAccessException e)
+        {
+            System.out.println("Successfully threw exception with error: " + e.getMessage());
+        }
+    }
+
+    public void TestLogin(UserService tester, String username, String password)  {
+        try
+        {
+            LoginRequest requestTest = new LoginRequest(username, password);
+            LoginResult result = tester.login(requestTest);
+            mAuthToken = result.authToken();
+            System.out.println("Successfully logged in as " + result.username() + " With auth token: " + result.authToken());
+        }
+        catch (DataAccessException e)
+        {
+            System.out.println("Successfully threw exception with error: " + e.getMessage());
+        }
+    }
+
+    public void TestLogout(UserService tester)  {
+        try
+        {
+            LogoutRequest requestTest = new LogoutRequest(mAuthToken);
+            tester.logout(requestTest);
+            System.out.println("Successfully logged out user");
+        }
+        catch (DataAccessException e)
+        {
+            System.out.println("Successfully threw exception with error: " + e.getMessage());
+        }
+    }
+
+    public void TestClearUsers(UserService tester)  {
+        tester.clearUsers();
+        System.out.println("Successfully cleared all users");
+    }
+}
