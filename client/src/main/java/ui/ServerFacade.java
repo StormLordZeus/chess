7174cdf88a5b2797639cb9ad56f9a4e6c89a0c8b
Christopher Zaugg <czauggt@byu.edu>
2025-11-10 12:45:12 -0700

    Wrote the ServerFacade class

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,123 @@
+package ui;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import Exception.ResponseException;
+
+import com.google.gson.Gson;
+import model.*;
+
+public class ServerFacade {
+    private final HttpClient mClient = HttpClient.newHttpClient();
+    private final String mServerUrl;
+
+    public ServerFacade(String aUrl)
+    {
+        mServerUrl = aUrl;
+    }
+
+    public RegisterResult register(RegisterRequest aRequest) throws ResponseException
+    {
+        var request = buildRequest("POST", "/user", aRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    public LoginResult login(LoginRequest aRequest) throws ResponseException {
+        var request = buildRequest("POST", "/session", aRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, LoginResult.class);
+    }
+
+    public void logout(LogoutRequest aRequest) throws ResponseException {
+        var request = buildRequest("DELETE", "/session", aRequest);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public ListGamesResult listGames(ListGamesRequest aRequest) throws ResponseException {
+        var request = buildRequest("POST", "/user", aRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, ListGamesResult.class);
+    }
+
+    public CreateGameResult createGame(CreateGameResult aRequest) throws ResponseException {
+        var request = buildRequest("POST", "/user", aRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, CreateGameResult.class);
+    }
+
+    public void joinGame(JoinGameRequest aRequest) throws ResponseException {
+        var request = buildRequest("POST", "/user", aRequest);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public void clear() throws ResponseException {
+        var request = buildRequest("POST", "/user", null);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    private HttpRequest buildRequest(String path, String method, Object body)
+    {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(mServerUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null)
+        {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object aRequest)
+    {
+        if (aRequest != null)
+        {
+            return BodyPublishers.ofString(new Gson().toJson(aRequest));
+        }
+        else
+        {
+            return BodyPublishers.noBody();
+        }
+    }
+
+
+    private HttpResponse<String> sendRequest(HttpRequest aRequest) throws ResponseException
+    {
+        try
+        {
+            return mClient.send(aRequest, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (Exception e)
+        {
+            throw new ResponseException(ResponseException.Code.ServerError, e.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> aResponse, Class<T> aResponseClass) throws ResponseException
+    {
+        var status = aResponse.statusCode();
+        if (status != 200)
+        {
+            var body = aResponse.body();
+            if (body != null)
+            {
+                throw ResponseException.fromJson(body);
+            }
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (aResponseClass != null)
+        {
+            return new Gson().fromJson(aResponse.body(), aResponseClass);
+        }
+        return null;
+    }
+
+}
